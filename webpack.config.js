e4ad4661e87fd1ee30554916ba8f4c83cfa1488c
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-08-09 19:50:38 -0700

    Adds webpack config

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require("path");
+var SRC_DIR = path.join(__dirname, "/components");
+var DIST_DIR = path.join(__dirname, "/public/dist");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env", "react"]
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["eslint-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              disable: true // webpack@2.x and newer
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
